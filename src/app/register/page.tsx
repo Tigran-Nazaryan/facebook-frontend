be3b9a887da655ae1564e8db4bff5e4ac53ee6aa
Tigran Nazaryan <tigran.nazaryan.18@gmail.com>
2025-09-08 19:23:50 +0400

    create login and register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegistrationForm } from "@/components/RegistrationForm";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <RegistrationForm />
+      </div>
+    </div>
+  );
+}
